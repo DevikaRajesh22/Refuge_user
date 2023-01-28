3e1db816c438c8a5596025b26e9274554e90ed80
Akshaya M V <akshaya1672@gmail.com>
2023-01-28 13:21:01 +0530

    final stage

--- lib/ui/screens/registration_screen.dart
+++ lib/ui/screens/registration_screen.dart
@@ -5,45 +5,132 @@ class RegistrationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      body: Column(children: [
-        Container(
-          width: 400,
-          height: 250,
-          color: Colors.blue,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Row(
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/regi.png',
+              fit: BoxFit.fill,
+              width: MediaQuery.of(context).size.width,
+              height: 250,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Row(
+                  children: const [
+                    Expanded(
+                      child: Divider(
+                        color: Color.fromARGB(255, 96, 96, 96),
+                        thickness: 1,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text("Register"),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: Divider(
+                        color: Color.fromARGB(255, 96, 96, 96),
+                        thickness: 1,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const Text(
+                "Create your account . It's free and only takes a minute"),
+            Row(
               children: const [
                 Expanded(
-                  child: Divider(
-                    color: Color.fromARGB(255, 96, 96, 96),
-                    thickness: 1,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'First name',
+                    ),
                   ),
                 ),
                 SizedBox(
-                  width: 10,
-                ),
-                Text("Register"),
-                SizedBox(
-                  width: 10,
+                  width: 15,
                 ),
                 Expanded(
-                  child: Divider(
-                    color: Color.fromARGB(255, 96, 96, 96),
-                    thickness: 1,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Last name',
+                    ),
                   ),
-                )
+                ),
               ],
             ),
-          ),
+            Row(
+              children: const [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Age',
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Gender',
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+                hintText: 'mail@user.com',
+              ),
+            ),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Password",
+                  hintText: "Enter Password"),
+            ),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: "Confirm Password",
+                  hintText: "Enter Password"),
+            ),
+            Row(
+              children: [
+                Container(
+                  width: 15,
+                  height: 15,
+                  color: const Color.fromARGB(255, 32, 118, 188),
+                ),
+                const Text('I accept  the terms of use & privacy '),
+              ],
+            ),
+            ElevatedButton(
+              child: const Text("Register"),
+              onPressed: () {},
+            ),
+            const Text('Already a user ? Login ')
+          ],
         ),
-        const Text("Create your account . It's free and only takes a minute")
-      ]),
-    ));
+      ),
+    );
   }
 }
