0d986342f4c4993f4785d644386b23a69ba976fd
Akshaya M V <akshaya1672@gmail.com>
2023-02-22 12:42:30 +0530

    emergency screen images inserted

--- lib/ui/screens/emerge_screen.dart
+++ lib/ui/screens/emerge_screen.dart
@@ -24,14 +24,144 @@ class EmergeScreen extends StatelessWidget {
         ),
       ),
       body: SafeArea(
-          child: Container(
-        height: 250,
-        width: 500,
-        decoration: const BoxDecoration(
-            color: Colors.blue,
-            borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30),
-                bottomRight: Radius.circular(30))),
+          child: Column(
+        children: [
+          Container(
+            height: 250,
+            width: 500,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30)),
+            ),
+            child: Image.asset(
+              'assets/images/light.png',
+              height: 50,
+              width: 50,
+            ),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 120,
+                width: 160,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    border: Border.all()),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Image.asset(
+                      'assets/images/blood.png',
+                      height: 30,
+                      width: 30,
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text('Blood Donation')
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Container(
+                height: 120,
+                width: 160,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    border: Border.all()),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Image.asset(
+                      'assets/images/camera.png',
+                      height: 60,
+                      width: 60,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const Text('Report Hazard')
+                  ],
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 120,
+                width: 160,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.all(Radius.circular(10)),
+                    border: Border.all()),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Image.asset(
+                      'assets/images/hand.png',
+                      height: 60,
+                      width: 60,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const Text('Care Team')
+                  ],
+                ),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Container(
+                height: 120,
+                width: 160,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    border: Border.all()),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Image.asset(
+                      'assets/images/van.png',
+                      height: 60,
+                      width: 60,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    const Text('Emergency Servies')
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       )),
     );
   }
