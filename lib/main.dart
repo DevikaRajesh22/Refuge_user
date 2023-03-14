e1cbf5875291319fc9ccf6c9bceaddb8043b2232
Akshaya M V <akshaya1672@gmail.com>
2023-03-14 20:56:04 +0530

    location screen completed without adding map

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:refuge_user/ui/screens/careteam_screen.dart';
 import 'package:refuge_user/ui/screens/family_screen.dart';
 import 'package:refuge_user/ui/screens/home_screen.dart';
 import 'package:refuge_user/ui/screens/home_screen_sections/dashboard_section.dart';
+import 'package:refuge_user/ui/screens/location_screen.dart';
 import 'package:refuge_user/ui/screens/login_screen.dart';
 import 'package:refuge_user/ui/screens/registration_screen.dart';
 import 'package:refuge_user/ui/screens/splash_screen.dart';
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RegistrationScreen(),
+      home: LocationScreen(),
     );
   }
 }
